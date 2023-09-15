fb2af5798e6c3ad150bc4bdb91e14e06c7c17bab
noel_linux <noel.eklof@hotmail.com>
2023-09-15 22:21:18 +0200

    test från for loop till matplots Func funktion

--- plotAniTest.py
+++ plotAniTest.py
@@ -1,40 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time as time
+from matplotlib.animation import FuncAnimation
+import time
 
+start = time.monotonic()
 
-x_value = 0
-y_value = 100
-fps = 60
-totTime = 5
-v = 0 
+fps = 30
+v = 20 
+theta = np.pi/3
 g = 9.82
+zero = 2*np.sin(theta)*v/g
+
+fig, ax = plt.subplots()
+ball = ax.scatter(0,0)
+
 
 def y_distence(v,t):
-    s = v*t + (g*t**2)/2
+    s = np.sin(theta)*v*t - (g*t**2)/2
     return s
 
-def velocety(v,t):
-    v_2 = v + g*t
-    return v_2
+def x_distence(v,t):
+    s = np.cos(theta)*v*t
+    return s
 
-start = time.time()
-for t in np.linspace(0, totTime, fps*totTime):
-    plt.clf()
-    plt.ylim(-10,110)
-    y_value -= y_distence(v,t)
-    plt.scatter(x_value, y_value)
-    plt.pause(time/(fps*totTime))
-    if y_value <= 0:
-        end = time.time()
-        break
+def animation_frame(t):
+    ax.cla()
+    plt.title("Projectile Motion")  
+    plt.grid()
+    ax.set_xlim(0,x_distence(v,zero)+10)
+    ax.set_ylim(0, x_distence(v,zero)+10)
+    ball = ax.scatter(x_distence(v,t),y_distence(v,t))
+    return ball 
 
+animation = FuncAnimation(fig, func=animation_frame, frames = np.arange(0, zero, 1/fps), interval = (1/fps)*1000)
+
+end = time.monotonic()
 plt.show()
+print(end-start)
 
-print(y_value)
-print(end - start)
-#xpoints = np.array([0, 6])
-#ypoints = np.array([0, 250])
-#
-#plt.plot(xpoints, ypoints)
-#plt.show()
+
