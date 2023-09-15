3cde5924f1fe959286d2fd1b7d9d8e6f80ba27ac
Noel eklof <noel.eklof@hotmail.com>
2023-09-15 12:54:27 +0200

    Testa med animationer

--- plotAniTest.py
+++ plotAniTest.py
@@ -1,13 +1,40 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import time as time
+
 
 x_value = 0
 y_value = 100
+fps = 60
+totTime = 5
+v = 0 
+g = 9.82
 
-for i in range(0, 10):
-    y_value = y_value - 10
-    print(y_value)
+def y_distence(v,t):
+    s = v*t + (g*t**2)/2
+    return s
+
+def velocety(v,t):
+    v_2 = v + g*t
+    return v_2
+
+start = time.time()
+for t in np.linspace(0, totTime, fps*totTime):
+    plt.clf()
+    plt.ylim(-10,110)
+    y_value -= y_distence(v,t)
     plt.scatter(x_value, y_value)
-    plt.pause()
+    plt.pause(time/(fps*totTime))
+    if y_value <= 0:
+        end = time.time()
+        break
 
 plt.show()
+
+print(y_value)
+print(end - start)
+#xpoints = np.array([0, 6])
+#ypoints = np.array([0, 250])
+#
+#plt.plot(xpoints, ypoints)
+#plt.show()
