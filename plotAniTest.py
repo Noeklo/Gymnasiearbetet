21dacbf9e842488a406d10105fda9311b29e30b5
noel_linux <noel.eklof@hotmail.com>
2023-09-15 22:59:45 +0200

    la till cirkel istället för scatter

--- plotAniTest.py
+++ plotAniTest.py
@@ -1,18 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Circle
 import time
 
 start = time.monotonic()
 
 fps = 30
-v = 20 
-theta = np.pi/3
+v = 10
+r=0.1
+theta = np.pi/4
 g = 9.82
 zero = 2*np.sin(theta)*v/g
 
 fig, ax = plt.subplots()
-ball = ax.scatter(0,0)
+#ball = ax.scatter(0,0)
+circle = Circle((0,0),r)
 
 
 def y_distence(v,t):
@@ -27,10 +30,12 @@ def animation_frame(t):
     ax.cla()
     plt.title("Projectile Motion")  
     plt.grid()
-    ax.set_xlim(0,x_distence(v,zero)+10)
-    ax.set_ylim(0, x_distence(v,zero)+10)
-    ball = ax.scatter(x_distence(v,t),y_distence(v,t))
-    return ball 
+    ax.set_xlim(0,x_distence(v,zero)+5)
+    ax.set_ylim(0, x_distence(v,zero)+5)
+#    ax.scatter(x_distence(v,t),y_distence(v,t))
+    circle = Circle((x_distence(v,t),y_distence(v,t)),r)
+    ax.add_patch(circle)
+
 
 animation = FuncAnimation(fig, func=animation_frame, frames = np.arange(0, zero, 1/fps), interval = (1/fps)*1000)
 
