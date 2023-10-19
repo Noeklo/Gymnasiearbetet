a71515f0c645fc0762a681f57a2a91242acb48f0
noel_linux <noel.eklof@hotmail.com>
2023-10-19 22:17:44 +0200

    stabil version

--- Backend/AnimationWriter.py
+++ Backend/AnimationWriter.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import FFMpegWriter
+import tkinter
 import time
 
 from CircleObj import CircleObj
@@ -18,7 +19,6 @@ class AnimationWriter:
     y = 0
     fps: int = 60
 
-    canvas1: Canvas = None 
     circle1: CircleObj = None 
 
     calc1: Calc = None
@@ -29,11 +29,15 @@ class AnimationWriter:
 
         return self.circle1.circle, 
 
-    def __init__(self):
-        None
+#    def __init__(self, canvas1: Canvas, widow: tkinter.Tk = None,):
+#        self.window = widow
+#        self.canvas1 = canvas1
+
+    def __init__(self, widow: tkinter.Tk = None,):
+        self.window = widow
 
     def generate_Animation(self,):
-        self.canvas1 = Canvas((6,6), self.fps, "Projectile Motion")
+        self.canvas1 = Canvas((6,6), self.fps, "Projectile Motion", self.window)
         self.circle1 = CircleObj(self.radius,self.mass,self.x_Velocity,self.y_Velocity,self.x,self.y)
 
         self.canvas1.ax.add_patch(self.circle1.circle)
@@ -48,8 +52,11 @@ class AnimationWriter:
                                  frames = np.arange(0,len(self.x_Cords)-1, 1),
                                  interval = 1000/self.canvas1.fps,
                                  blit = True)
-                                 
-        self.canvas1.tkCanvas.show(block=False)
+        #self.canvas1.draw()
+        plt.show(block=False)
+        #self.canvas1.ax.plot()
+
+        print("generated animation")
 
     def stop_Animation(self,):
         plt.close(self.canvas1.fig)
--- Backend/test.py
+++ Backend/test.py
@@ -1,5 +1,30 @@
 from AnimationWriter import AnimationWriter
+from tkinter import *
+
 
 ani = AnimationWriter()
 
 ani.generate_Animation()
+
+#window = Tk() 
+#  
+#ani = AnimationWriter(window)
+## setting the title  
+#window.title('Plotting in Tkinter') 
+#  
+## dimensions of the main window 
+#window.geometry("500x500") 
+#  
+## button that displays the plot 
+#plot_button = Button(master = window,  
+#                     command = ani.generate_Animation(), 
+#                     height = 2,  
+#                     width = 10, 
+#                     text = "Plot") 
+#  
+## place the button  
+## in main window 
+#plot_button.pack() 
+#  
+## run the gui 
+#window.mainloop() 
